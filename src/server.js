cead59612ef0289432240bf4a39146f87b7625cb
Kylee Tilley <kyleetilley@gmail.com>
2019-02-17 17:52:10 -0600

    Add morgan logging

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import morgan from "morgan";
 
 const server = express();
+
+server.use(morgan("tiny"));
 
 server.get(`/`, (_, res) => {
   res.send("Analytics Required Trainer Service");
