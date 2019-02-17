249270c0cd73bcf1cd89b857174ce44576884b5a
Kylee Tilley <kyleetilley@gmail.com>
2019-02-17 03:29:38 -0600

    Implement basic server

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ## Scripts
 
 - `build`: Builds to `dist/index.js`
+- `start`: Start server without building
+- `start:cold`: Start build then start server
 - `test`: Runs all tests
 - `preview`: Shows the files that will be bundled with `publish`
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-export default {};
+import server from "./server";
+
+const port = 3000;
+
+server.get(`/`, (_, res) => {
+  res.send("Analytics Required Trainer Service");
+});
+
+server.listen(port, () => console.log(`Trainer started on port: ${port}!`));
