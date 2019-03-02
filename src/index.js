1b49335d58d58baa9544d385587a813c7bd9b986
Kylee Tilley <kyleetilley@gmail.com>
2019-03-01 21:19:02 -0600

    Really fixing port

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import server from "./server";
 
-const port = process.env.NODE_ENV ? 80 : 3000;
+const port = process.env.NODE_ENV === "production" ? process.env.PORT : 3000;
 
 server.listen(port, () => console.log(`Trainer started on port: ${port}!`));
